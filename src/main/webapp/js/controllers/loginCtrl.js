d83f7d45bf373faabf16f2ace0ff72893caceddd
gamecreator94 <rawsonmb@gmail.com>
2017-09-08 13:11:10 -0400

    Added documentation for js files and modified createGame.html to store the amount of players and timer in the game parameter for the function game.

--- src/main/webapp/js/controllers/loginCtrl.js
+++ src/main/webapp/js/controllers/loginCtrl.js
@@ -1,6 +1,12 @@
 var wolfsta = angular.module("wolfsta");
 
-
+/**
+ * loginCtrl is connected to the login.html.
+ * The login functions sends the player's username and password input to the java backend
+ * using the $http /session/login then it takes the response and checks if it is not null.
+ * If it is not null it changes the view to joinGame.html and stores the token and id from the response
+ * else it alerts the user that they have inputted invalid information.
+ */
 wolfsta.controller('loginCtrl', function($scope, $http, $location){
     
      $scope.login = function(player){
